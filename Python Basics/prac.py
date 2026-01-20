6e85ff3d59fd073338ffdc03a4aeb1fdbf91b5a9
dev2bangladeshisoftware <dev2.bangladeshisoftware@gmail.com>
2026-01-20 19:42:54 +0600

    push

---
+++ Python Basics/prac.py
@@ -0,0 +1,51 @@
+name = "DEV Two"
+age = 32
+
+#print("Hello " + name + ", you are " + str(age) + " years old.")
+
+x = 10
+y = 12.4
+z = "23"
+
+#print(f"x is {type(x).__name__}")
+#print(f"y is {type(y).__name__}")
+#print(f"z is {type(z).__name__}")
+
+
+a = 11 
+b = 3
+ 
+#print("sum:", a + b)
+#print("difference:", a- b)
+#print("product:", a * b)
+#print("division:", a / b)
+print("floor division:", a // b)
+
+
+devs = {"name": "Amin Islam", "age": 28, "skill": "Python"}
+
+#print("Name:", devs["name"], "Age:", devs["age"], "skill:", devs["skill"])
+
+
+a = 100
+b = 99.9
+c = "Python"
+d = True
+
+#print(f"a -> {type(a).__name__}")
+#print(f"b -> {type(b).__name__}")
+#print(f"c -> {type(c).__name__}")
+#print(f"d -> {type(d).__name__}")
+
+
+#print("Hello world " + name + " You are " + str(age) + " Years old")
+
+d = True
+
+print(f"d is {type(d).__name__}")
+
+
+DUBUG = True
+
+if DUBUG: 
+    print(f"Debugging is enabled for {type(a).__name__}")

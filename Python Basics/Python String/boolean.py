f3ba9edcd897547a06541fe0d47be0583d651d54
dev2bangladeshisoftware <dev2.bangladeshisoftware@gmail.com>
2026-01-24 19:20:03 +0600

    push

---
+++ Python Basics/Python String/boolean.py
@@ -0,0 +1,70 @@
+#One-line takeaway
+
+#In Python, objects can be empty, and empty means False.
+
+age = 10
+
+#print(age > 22)
+
+#print(age < 22)
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+#print(bool("Hello"))
+#print(bool(15))
+
+
+x = "Hello"
+y = 15
+
+#print(bool(x))
+print(bool(y))
+#
+
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+
+"""Some Values are False
+In fact, there are not many values that evaluate to False, except empty values, such as (), [], {}, "", the number 0, and the value None. And of course the value False evaluates to False."""
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+
+class myclass2():
+   def __len__(self):
+      return 5
+
+myobj2 = myclass2()
+print(bool(myobj2))
+
+z  = 0
+if z:
+    print("True")
+else:
+    print("False")
+
+
+
+class myClass():
+    def __len__(self):
+        return 5
+
+myJob = myClass()
+print(bool(myJob))
+
+class aminVai():
+    def __len__(self):
+        return 10
+    
+myAmin = aminVai()
+print(bool(myAmin))
---
+++ Python Basics/Python String/operators.py
@@ -0,0 +1,20 @@
+#Python Arithmetic Operators
+
+x = 200
+y = 5
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+
+result = x - (x * y / 100)
+print(result)
+print(x - (x * y / 100))
+
+
+print(x // y)  # Floor division there is no float value
+print(x ** y)  # Exponentiation operator like power 
+
+

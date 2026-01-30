7e950bacbe953191aca8eff10f3cb4e501e852dc
Md Amin Islam <programmeraminbd@gmail.com>
2026-01-30 16:27:31 +0600

    push

---
+++ Python_Basics/Python_List/listmethod.py
@@ -0,0 +1,15 @@
+#Add an element to the fruits list:
+
+#append()	Adds an element at the end of the list
+
+fruits = ["Apple", "Mango", "Orange", "Cherry"]
+fruits.append("orange")
+
+list = ["Amin", "Rahim", "Karim",]
+
+#fruits.append(list)
+
+print(fruits)
+
+#clear() Removes all the elements from the list
+
--- Python_Basics/Python_List/pylistadd.py
+++ Python_Basics/Python_List/pylistadd.py
@@ -25,3 +25,12 @@ thislist = ["apple", "banana", "cherry"]
 thistuple = ("kiwi", "orange")
 thislist.extend(thistuple)
 print(thislist)
+
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x)
+
+print(list1)

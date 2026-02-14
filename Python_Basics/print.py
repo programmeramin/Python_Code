c21e347440a303681e337071d2f447ad3dbf04ff
dev2bangladeshisoftware <dev2.bangladeshisoftware@gmail.com>
2026-02-14 19:38:06 +0600

    update print input  type

---
+++ Python_Basics/print.py
@@ -0,0 +1,11 @@
+print("20-24-2025")
+print("20", "23", "2034", sep="/")
+print("20", "23", "2034", sep="-")
+
+print("Hello", end=" ")
+print("How are you")
+
+print("my name is something\n i am 100 years old")
+
+name = input("Enter your name")
+number = input("Enter your number")

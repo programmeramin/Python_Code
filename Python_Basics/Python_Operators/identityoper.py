fe15592fed3ac95b353de776abc9e74d279bcef4
dev2bangladeshisoftware <dev2.bangladeshisoftware@gmail.com>
2026-02-18 17:02:09 +0600

    code update

---
+++ Python_Basics/Python_Operators/identityoper.py
@@ -0,0 +1,29 @@
+# 🆔 7️⃣ Identity Operators
+
+# Memory location check করার জন্য
+
+# is
+
+# is not
+
+#মানে: দুইটা variable কি একই object কে point করছে?
+
+
+a = 10
+b = 10
+
+print(a is b)
+
+
+
+x = [1, 2, 3]
+y = [1, 2, 3]
+
+print(x is y)
+
+
+a = [1, 2]
+b = [1, 2]
+
+print(a is not b)
+

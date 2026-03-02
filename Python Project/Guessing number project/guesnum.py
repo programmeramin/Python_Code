4f2fc227ebe972cb24f513d4ece269ee709836d9
Md Amin Islam <programmeraminbd@gmail.com>
2026-03-02 10:09:43 +0600

    project file add

---
+++ Python Project/Guessing number project/guesnum.py
@@ -0,0 +1,27 @@
+import random
+
+secret_number = random.randint(1, 10)
+
+import random
+
+# Random number generate (1 to 100)
+secret_number = random.randint(1, 100)
+
+import random
+
+# Random number generate (1 to 100)
+secret_number = random.randint(1, 100)
+
+print("🎮 Welcome to Number Guessing Game!")
+print("I have selected a number between 1 and 10")
+
+while True:
+    guess = int(input("Enter your guess: "))
+
+    if guess > secret_number:
+        print("Too High! 📈")
+    elif guess < secret_number:
+        print("Too Low! 📈")
+    else:
+        print(f"🎉 Correct! You guessed in attempts.")
+        break

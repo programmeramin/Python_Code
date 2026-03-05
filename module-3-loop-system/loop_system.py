3c8c70d311373d3f841ddede849277de656d12a4
dev2bangladeshisoftware <dev2.bangladeshisoftware@gmail.com>
2026-03-05 15:49:42 +0600

    update

---
+++ module-3-loop-system/loop_system.py
@@ -0,0 +1,10 @@
+#📌 Step 2: Program Introduction
+
+print("Welcome to Smart Task Repetition System")
+
+#📌 Step 3: Task Input
+task_name = input("Enter your task name (Example: Study Python, Practice Math): ")
+repeat_times = int(input("How many times do you want to repeat this task today? "))
+
+
+#📌 Step 4: Using a for Loop

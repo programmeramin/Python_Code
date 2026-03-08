30befdd98449794b5e9189f47820e85871cd4290
dev2bangladeshisoftware <dev2.bangladeshisoftware@gmail.com>
2026-03-08 11:39:35 +0600

    update code

---
+++ Python_Basics/Python_Math/Math.py
@@ -0,0 +1,54 @@
+a = -20
+print(abs(a))
+
+
+b = 3.1416
+
+print(b)
+print(round(b, 2))
+
+
+c = [4, 5, 6, 77, 8, 9]
+
+d = 23
+
+print(max(c))
+
+
+print(max(2,3,4,65,34,23,654,))
+print(min(2,3,4,65,34,23,654,))
+
+
+print(divmod(11, 5))
+
+
+#id id is being work where is locate variable
+x = 10
+print(id(x))
+
+
+# help(print)
+
+
+#callable
+#callable(print)
+
+#eval
+print(eval("23+34"))
+
+
+print(sum((2,4,5,6,3,256)))
+
+
+# sorted
+print(sorted("cabsals"))
+
+text = "python"
+print(text[slice(0,4)])
+
+
+total = 1
+
+for i in range(1,5):
+    total += i
+    print(total)

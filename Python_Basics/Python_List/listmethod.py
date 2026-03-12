c99bd4d01b14364bf0b97e01da6cefe9f65dc3b3
Md Amin Islam <programmeraminbd@gmail.com>
2026-03-12 22:34:21 +0600

    code updated

--- Python_Basics/Python_List/listmethod.py
+++ Python_Basics/Python_List/listmethod.py
@@ -38,3 +38,35 @@ print(name)
 name.append("Raihan")
 print(name)
 print(name[0 : 3])
+
+
+#step slicing
+a = [1,2,3,4,5,6,7,8,9]
+
+print(a[::2])
+
+#reverse print
+print(a[::-1])
+print(a[-4:-1])
+
+# 2 list with sum
+b = [1,3,4,5]
+c = [6,7,8,9]
+
+b.extend(c)
+print(b)
+
+# specific data remove
+number = [1, 23,45,6,45,5,6]
+
+number.pop(5)
+print(number)
+
+#list full data remove ok
+student = [23, 23 , "amin islam", 34]
+student.clear()
+print(student)
+
+student5 = [23, 23 , "amin islam", 34]
+print(student5.index("amin islam"))
+print(student5.count(34))
